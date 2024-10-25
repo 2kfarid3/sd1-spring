13f502e1bd0cc7fda3d89fd8178837cc08eccdec
2kfarid3 <fkarimli047@gmail.com>
2024-10-25 10:05:43 +0400

    first commit

---
+++ src/test/java/org/example/sdfspring/SdfSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sdfspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdfSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
